79f4662cf29ccf7cc276d722f3d5765dc67f8316
glyfspace <rishipandey125@gmail.com>
2025-03-19 11:10:58 -0700

    implemented negative guidance - could use some work though

--- utils/attention_functions.py
+++ utils/attention_functions.py
@@ -1,6 +1,6 @@
 from comfy.ldm.modules.attention import default, optimized_attention, optimized_attention_masked
 
-from .style_functions import adain, swapping_attention
+from .style_functions import adain, swapping_attention, get_attention
 
 class VisualStyleProcessor(object):
     def __init__(self, 
@@ -11,7 +11,7 @@ class VisualStyleProcessor(object):
         adain_keys: bool = True,
         adain_values: bool = False,
         nvqg_enabled: bool = True,      # new: enable NVQG
-        nvqg_weight: float = 0.5         # new: strength of negative guidance
+        nvqg_weight: float = 1         # new: strength of negative guidance
     ):
         self.module_self = module_self
         self.style_intensity = style_intensity
@@ -43,23 +43,23 @@ class VisualStyleProcessor(object):
             # Perform swapping on k,v using reference style
             k, v = swapping_attention(k, v, style_intensity=self.style_intensity)
         
+        q_style = get_attention(q,0)
+        k_style = get_attention(k,0)
+        v_style = get_attention(v,0)
+
+        q_text = get_attention(q,1)
+        k_text = get_attention(k,1)
+        v_text = get_attention(v,1)
+
         # Compute positive attention using the modified q,k,v
-        if mask is None:
-            positive_attn = optimized_attention(q, k, v, self.module_self.heads)
-        else:
-            positive_attn = optimized_attention_masked(q, k, v, self.module_self.heads, mask)
+
+        positive_attn = optimized_attention(q, k_style, v_style, self.module_self.heads)
 
         # If NVQG is enabled, compute a negative branch and combine.
         if self.nvqg_enabled:
-            # Compute a "negative" query.
-            # Here we simply recompute the raw query without applying adain (or with a different transformation).
-            q_negative = self.module_self.to_q(x)
+
             # Optionally, you could skip any style normalization on q_negative.
-            if mask is None:
-                negative_attn = optimized_attention(q_negative, k, v, self.module_self.heads)
-            else:
-                negative_attn = optimized_attention_masked(q_negative, k, v, self.module_self.heads, mask)
-            
+            negative_attn = optimized_attention(q, k_text, v_text, self.module_self.heads)            
             # Combine the two attention outputs.
             # This follows a CFG-style formulation: final = (1 + w)*positive - w*negative.
             combined_attn = (1 + self.nvqg_weight) * positive_attn - self.nvqg_weight * negative_attn
