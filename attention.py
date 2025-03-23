8d72e85927b7e732e3deddbece0889b75d10a316
Raphael Walker <slickytail.mc@gmail.com>
2025-03-23 16:52:23 +0100

    code cleanup, and add workflows

--- attention.py
+++ attention.py
@@ -3,7 +3,7 @@ from comfy.ldm.modules.attention import optimized_attention
 
 
 # this code adapted from cubiq's ipadapter implementation
-def patch_model_block(model, kwargs, key):
+def patch_model_block(model, key, **kwargs):
     to = model.model_options["transformer_options"].copy()
     if "patches_replace" not in to:
         to["patches_replace"] = {}
@@ -22,7 +22,7 @@ def patch_model_block(model, kwargs, key):
 
 class Attn1Replace:
     def __init__(self, **kwargs):
-        self.kwargs = kwargs
+        self.args = kwargs
 
     def __call__(self, q, k, v, extra_options):
         dtype = q.dtype
@@ -33,9 +33,9 @@ class Attn1Replace:
             else 999999999.9
         )
         if (
-            self.kwargs.get("sigma_end", 0.0)
+            self.args.get("sigma_end", 0.0)
             <= sigma
-            <= self.kwargs.get("sigma_start", 999999999.9)
+            <= self.args.get("sigma_start", 999999999.9)
         ):
             # List of [0, 1], [0], [1], ...
             # 0 means conditional, 1 means unconditional
@@ -45,11 +45,11 @@ class Attn1Replace:
             k_ref = k[0]
             v_ref = v[0]
             # if we're doing NVQG, then the unconditional images should use q_ref instead
-            if self.kwargs.get("swap_uncond", False):
+            if self.args.get("swap_uncond", False):
                 q = torch.stack(
                     [[q_i, q_ref][c] for q_i, c in zip(q, cond_or_uncond)], dim=0
                 )
-            if self.kwargs.get("swap_cond", False):
+            if self.args.get("swap_cond", False):
                 # swap the keys and values in the conditional generation
                 k = torch.stack(
                     [[k_ref, k_i][c] for k_i, c in zip(k, cond_or_uncond)], dim=0
--- styleguide.py
+++ styleguide.py
@@ -1,6 +1,6 @@
+from typing import Callable
 import comfy
 import torch
-from time import sleep
 
 from .attention import patch_model_block
 
@@ -34,18 +34,24 @@ def merge_cond(c: dict, cond: list) -> dict:
     y = torch.cat((y, c["y"][0:1, 1280:]), dim=1)
     c["y"] = torch.cat((y, c["y"]), dim=0)
     # combine the controlnet stuff
-    # we'll create a zero vector on the 0th dim
-    # NOTE: I think the "correct" approach is to zero out the controlnet
+    # we'll zero out the activations on the new component
+    # we're also going to zero out the controlnet on the uncond inputs
+    # this increases the strength of the controlnet. otherwise it gets drowned out by the style injection.
     if c.get("control", None) is not None:
+        # the batch indices to zero out
         uncond_indices = torch.tensor(
             [i for i, t in enumerate(c["transformer_options"]["cond_or_uncond"]) if t]
         )
         new = {}
-        for k, v in c["control"].items():
+        # the control data is a mapping from [in/middle/out] to lists of tensors: activations to be added inside the model
+        old: dict[str, list[torch.Tensor]]
+        old = c["control"]
+        for k, v in old.items():
             new[k] = []
             for t in v:
                 out = torch.cat((torch.zeros_like(t[0]).unsqueeze(0), t), dim=0)
-                out[uncond_indices, ...] = 0
+                # zero out the uncond
+                out[uncond_indices] = 0.0
                 new[k].append(out)
         c["control"] = new
     return c
@@ -80,6 +86,7 @@ class ApplyVisualStyle:
                         "default": 1.0,
                         "min": 0.0,
                         "max": 1.0,
+                        "step": 0.01,
                         "tooltip": "The end time % for the stylization.",
                     },
                 ),
@@ -95,12 +102,12 @@ class ApplyVisualStyle:
     def apply_visual_style_prompt(
         self,
         model: comfy.model_patcher.ModelPatcher,
-        reference_latent,
-        reference_cond,
-        nvqg_enabled,
-        skip_output_layers=0,
-        start_percent=0.0,
-        end_percent=1.0,
+        reference_latent: dict,
+        reference_cond: list,
+        nvqg_enabled: bool,
+        skip_output_layers: int = 0,
+        start_percent: float = 0.0,
+        end_percent: float = 1.0,
     ):
 
         model = model.clone()
@@ -122,61 +129,65 @@ class ApplyVisualStyle:
         assert len(reference_cond) == 1
 
         # patch the model's forward function to noise and add the reference samples
-        def add_reference(apply_model, p):
+        def add_reference(apply_model: Callable, p: dict):
             x = p["input"]
             # t is actually the sigma, not the index
             t = p["timestep"]
             sigma = t.detach().cpu()[0].item()
-            if sigma_end <= sigma <= sigma_start:
-                visual = reference_samples.to(x.device)
-                # combine the conds
-                c = merge_cond(p["c"], reference_cond)
-                # noise the reference latent ("stochastic encoding")
-                visual = visual + torch.randn_like(visual) * sigma
-                # add it at batch index 0
-                # at this point we're past the layer where a batch can be broken up
-                # so we can safely add a new element to the batch and know it won't cause problems
-                x = torch.concat((visual, x), dim=0)
-                # add the timestep
-                t = torch.cat((t[0:1], t), dim=0)
-                out = apply_model(x, t, **c)
-                # remove the useless denoised reference image
-                out = out[visual.shape[0] :]
-            else:
-                out = apply_model(x, t, **p["c"])
+            # skip the reference injection at some timesteps
+            if not (sigma_end <= sigma <= sigma_start):
+                return apply_model(x, t, **p["c"])
+
+            visual = reference_samples.to(x.device)
+            # combine the conds
+            c = merge_cond(p["c"], reference_cond)
+            # noise the reference latent ("stochastic encoding")
+            visual = visual + torch.randn_like(visual) * sigma
+            # add it at batch index 0
+            # at this point we're past the layer where a batch can be broken up
+            # so we can safely add a new element to the batch and know it won't cause problems
+            x = torch.concat((visual, x), dim=0)
+            # add the timestep
+            t = torch.cat((t[0:1], t), dim=0)
+            out = apply_model(x, t, **c)
+            # remove the useless denoised reference image
+            out = out[visual.shape[0] :]
             return out
 
+        # the unet wrapper is the innermost wrapper around the model
+        # doing the injection here means that we are less flexible wrt what model/conds are used
+        # because we have to convert them to the input format ourselves
+        # but it also gives us a lot of control over the performance:
+        # comfy won't waste time running the sampler or controlnets on the reference image
+        # and we guarantee that it won't be run in a separate batch from the cond/uncond images.
         model.set_model_unet_function_wrapper(add_reference)
 
         # according to the paper, the NVQ is added in the input and middle blocks
+        # we add it to input and middle, and also to the out blocks (up to the skip index)
         if nvqg_enabled:
             # patch input blocks
-            for id in [4, 5, 7, 8]:  # id of input_blocks that have cross attention
+            for id in [4, 5, 7, 8]:  # id of input_blocks that have self attention
                 block_indices = (
                     range(2) if id in [4, 5] else range(10)
                 )  # transformer_depth
                 for index in block_indices:
                     patch_model_block(
                         model,
-                        {
-                            "swap_uncond": True,
-                            "swap_cond": False,
-                            "sigma_start": sigma_start,
-                            "sigma_end": sigma_end,
-                        },
                         ("input", id, index),
+                        swap_uncond=True,
+                        swap_cond=False,
+                        sigma_start=sigma_start,
+                        sigma_end=sigma_end,
                     )
             # patch middle blocks
             for index in range(10):
                 patch_model_block(
                     model,
-                    {
-                        "swap_uncond": True,
-                        "swap_cond": False,
-                        "sigma_start": sigma_start,
-                        "sigma_end": sigma_end,
-                    },
                     ("middle", 1, index),
+                    swap_uncond=True,
+                    swap_cond=False,
+                    sigma_start=sigma_start,
+                    sigma_end=sigma_end,
                 )
         # patch output blocks
         blocknum = 0
@@ -189,13 +200,12 @@ class ApplyVisualStyle:
                 if blocknum >= skip_output_layers:
                     patch_model_block(
                         model,
-                        {
-                            "swap_uncond": nvqg_enabled and not swap_cond,
-                            "swap_cond": swap_cond,
-                            "sigma_start": sigma_start,
-                            "sigma_end": sigma_end,
-                        },
                         ("output", id, index),
+                        # the blocks that are skipped are included in the NVQG query injection
+                        swap_uncond=nvqg_enabled and not swap_cond,
+                        swap_cond=swap_cond,
+                        sigma_start=sigma_start,
+                        sigma_end=sigma_end,
                     )
                 blocknum += 1
 
